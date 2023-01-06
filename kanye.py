c874637ea880ebe705591b58fbe5dafadfa9d254
wtrknght <116600901+wtrknght@users.noreply.github.com>
2023-01-06 21:47:42 +0000

    Update kanye.py

--- kanye.py
+++ kanye.py
@@ -1,8 +1,10 @@
 import random
-g = open('quotes.txt', 'r')
-f = g.readlines()
-t = []
-for line in f:
-	t.append(line)
-j = random.choice(t)
-print(j)
+def printquote():
+	g = open('quotes.txt', 'r')
+	f = g.readlines()
+	t = []
+	for line in f:
+		t.append(line)
+	j = random.choice(t)
+	print(j)
+	g.close()
