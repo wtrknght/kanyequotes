864764a767a26198a9d13b5997eb139a97762acc
1 YiB <1YiB@proton.me>
2023-01-09 21:09:46 +0530

    Update main.py

--- src/kanye/main.py
+++ src/kanye/main.py
@@ -6,10 +6,12 @@ import asyncio
 client = httpx.AsyncClient(base_url="https://api.kanye.rest")
 
 async def _get_data() -> str:
-
     res = (await client.get('/text')).content
     return res.decode('utf-8')
 
 async def GetQuote(n:int) -> list[str]:
     awaitables = [_get_data() for i in range(n)]
-    return await asyncio.gather(*awaitables)
+    try:
+        return await asyncio.gather(*awaitables)
+    finally:
+        await client.aclose() 
